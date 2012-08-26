cc4182a3b63edcd6fbefcb047c312f01e208638d
Shannon <shannonskipper@gmail.com>
2012-08-26 16:54:06 -0700

    require 'rubygems' for Ruby-1.8

--- Sinatra.app/Contents/MacOS/app.sh
+++ Sinatra.app/Contents/MacOS/app.sh
@@ -7,6 +7,6 @@ if [ ! -f sinatra ]
 	ruby -e "require 'rubygems/dependency_installer'; install_dir = File.writable?(Gem.dir) ? Gem.dir : Gem.user_dir; Gem::DependencyInstaller.new(:install_dir => install_dir).new.install 'sinatra'; Gem.refresh"
 fi
 
-ruby -e 'require "sinatra"; get("/") { "hrmm" }'&
+ruby -e 'require "rubygems"; require "sinatra"; get("/") { "hrmm" }'&
 
 ./apache-callback-mac -url "http://localhost:4567"
